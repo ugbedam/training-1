afbccfab00ab5dd33d3796caecc30d2a880f1615
patDevNG <cgrleadership@gmail.com>
2019-04-27 15:10:37 +0100

    first Javascript class

---
+++ assets/js/training.js
@@ -0,0 +1,33 @@
+// let Instructor = 'Patrick';
+// let TrainningCenter = 'Projaro';
+
+// console.log( Instructor +" " +"is and Instructor in "+""+ TrainningCenter);
+
+// Instructor ='Mr Emmanuel';
+// console.log(Instructor);
+
+// Instructor = TrainningCenter;
+// console.log(Instructor);
+
+
+// Calculating Area of a Rectangle
+// let breath = 12;
+// let length =10;
+// let Area = length * breath;
+// console.log(Area);
+// let num1 = document.getElementById("Number1").addEventListener(onclick,getName());
+// getName=()=>{
+//     let 
+// }
+// console.log(num1);
+
+let age =10;
+if(age>18){
+    console.log('welcome');
+    
+}else{
+    console.log('Go away');
+    
+}
+
+
